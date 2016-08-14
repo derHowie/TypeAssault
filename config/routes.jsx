49c74b64340d048315632e54dbda5049ef49c2e1
Christopher Howard <derhowiedesigns@gmail.com>
2016-08-14 13:46:26 -0500

    removed axios

--- config/routes.jsx
+++ config/routes.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {Router, Route, IndexRoute, hashHistory} from 'react-router';
-
 import App from '../components/App.jsx';
 import Game from '../components/containers/Game.jsx';
 
